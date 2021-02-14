70898ca290a67b9eaedad7a4075e53ad234a954d
ShaqTM <shaq@mail.ru>
2021-02-14 20:15:15 +1000

    2

--- bakery.go
+++ bakery.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	dbservice "bakery/dbService"
-	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -13,21 +12,24 @@ func main() {
 	mdb := dbservice.MDB{}
 	mdb.InitDatabase()
 
-	http.Handle("/", handleRoot(mdb))
-	http.Handle("/writeMaterial", UpdateData(mdb))
-	http.Handle("/readMaterial", handleChart(mdb))
-	http.Handle("/getDataArray", handlegetDataArray(mdb))
-	http.Handle("/getDeviceList", handleDeviceList(mdb))
-	http.Handle("/getParameters", handleGetParameters(mdb))
+	http.Handle("/api/writeunit", writeunit(mdb))
+	//	http.Handle("/writeMaterial", UpdateData(mdb))
+	//	http.Handle("/readMaterial", handleChart(mdb))
+	//	http.Handle("/getDataArray", handlegetDataArray(mdb))
+	//	http.Handle("/getDeviceList", handleDeviceList(mdb))
+	//	http.Handle("/getParameters", handleGetParameters(mdb))
 
-	http.ListenAndServe(":5000", nil)
+	http.ListenAndServe(":9000", nil)
 	for {
 
 	}
 }
 
-func handleRoot(mdb mdatabase.MDB) http.Handler {
+func writeunit(mdb dbservice.MDB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var value map[string]interface{}
+
+		mdb.UpdateData("units")
 		options := ""
 		device_list := mdb.Get_devices()
 		for _, device_name := range device_list {
@@ -38,52 +40,52 @@ func handleRoot(mdb mdatabase.MDB) http.Handler {
 	})
 }
 
-func handlegetLastData(mdb mdatabase.MDB) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		device_name := r.URL.Query().Get("device")
-		datetime := r.URL.Query().Get("datetime")
-		fmt.Println(datetime)
-		data := mdb.Get_last_data(device_name, datetime)
-		fmt.Println(data)
-		fmt.Fprintf(w, data)
-	})
-}
+//func handlegetLastData(mdb mdatabase.MDB) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		device_name := r.URL.Query().Get("device")
+//		datetime := r.URL.Query().Get("datetime")
+//		fmt.Println(datetime)
+//		data := mdb.Get_last_data(device_name, datetime)
+//		fmt.Println(data)
+//		fmt.Fprintf(w, data)
+//	})
+//}
 
-func handlegetDataArray(mdb mdatabase.MDB) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		device_name := r.URL.Query().Get("device")
-		datetime1 := r.URL.Query().Get("datetime1")
-		datetime2 := r.URL.Query().Get("datetime2")
-		data := mdb.Get_data_array(device_name, datetime1, datetime2)
+//func handlegetDataArray(mdb mdatabase.MDB) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		device_name := r.URL.Query().Get("device")
+//		datetime1 := r.URL.Query().Get("datetime1")
+//		datetime2 := r.URL.Query().Get("datetime2")
+//		data := mdb.Get_data_array(device_name, datetime1, datetime2)
+//
+//		fmt.Fprintf(w, data)
+//	})
+//}
+//func handleChart(mdb mdatabase.MDB) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		options := ""
+//		device_list := mdb.Get_devices()
+//		for _, device_name := range device_list {
+//			options = options + fmt.Sprintf(optionHTML, device_name, device_name)
+//		}
+//		fmt.Println(options)
+//		fmt.Fprintf(w, chartHTML, options)
+//	})
+//}
 
-		fmt.Fprintf(w, data)
-	})
-}
-func handleChart(mdb mdatabase.MDB) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		options := ""
-		device_list := mdb.Get_devices()
-		for _, device_name := range device_list {
-			options = options + fmt.Sprintf(optionHTML, device_name, device_name)
-		}
-		fmt.Println(options)
-		fmt.Fprintf(w, chartHTML, options)
-	})
-}
-
-func handleDeviceList(mdb mdatabase.MDB) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		device_list := mdb.Get_devices()
-		data, _ := json.Marshal(device_list)
-		fmt.Fprintf(w, string(data))
-	})
-}
-func handleGetParameters(mdb mdatabase.MDB) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		device_name := r.URL.Query().Get("device")
-		datetime1 := r.URL.Query().Get("datetime1")
-		datetime2 := r.URL.Query().Get("datetime2")
-		data := mdb.Get_parameters(device_name, datetime1, datetime2)
-		fmt.Fprintf(w, data)
-	})
-}
+//func handleDeviceList(mdb mdatabase.MDB) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		device_list := mdb.Get_devices()
+//		data, _ := json.Marshal(device_list)
+//		fmt.Fprintf(w, string(data))
+//	})
+//}
+//func handleGetParameters(mdb mdatabase.MDB) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		device_name := r.URL.Query().Get("device")
+//		datetime1 := r.URL.Query().Get("datetime1")
+//		datetime2 := r.URL.Query().Get("datetime2")
+//		data := mdb.Get_parameters(device_name, datetime1, datetime2)
+//		fmt.Fprintf(w, data)
+//	})
+//}
--- ui/src/main.js
+++ ui/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+axios.defaults.withCredentials = false
+axios.defaults.baseURL = 'http://127.0.0.1:9000';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
