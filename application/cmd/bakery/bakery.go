d34d26b25d978e1251c34af515cff587e6f42074
shaq <shaq@mail.ru>
2023-04-26 17:06:56 +1000

    Версию для работы под windows в виде службы перенес в ветку win-master. В этой ветке версия без возможности рабоы в виде службы.

--- application/cmd/bakery/bakery.go
+++ application/cmd/bakery/bakery.go
@@ -4,13 +4,9 @@ import (
 	"bakery/application/internal/app"
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
 
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/sys/windows/svc"
-	"golang.org/x/sys/windows/svc/eventlog"
 )
 
 func usage(errmsg string) {
@@ -25,75 +21,8 @@ func usage(errmsg string) {
 func main() {
 
 	log := logrus.New()
-	inService, err := svc.IsWindowsService()
-	if err != nil {
-		log.Fatalf("failed to determine if we are running in service: %v", err)
-	}
-	app := app.New(inService, log)
-	if inService {
-
-		dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-		logfile := app.Service.Name + ".log"
-		if err == nil {
-			logfile = dir + `\` + logfile
-		}
-		fmt.Println(dir)
-		f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-		if err != nil {
-			elog, elogErr := eventlog.Open(app.Service.Name)
-			if elogErr != nil {
-				return
-			}
-			elog.Error(1, fmt.Sprintf("error opening file: %v", err))
-			elog.Close()
-			return
-		}
-		defer f.Close()
-		// Output to stderr instead of stdout, could also be a file.
-		log.SetOutput(f)
-		app.Start()
-		return
-	} else {
-		log.SetOutput(os.Stdout)
-	}
+	app := app.New(log)
+	log.SetOutput(os.Stdout)
 	log.Info("Started")
-	if len(os.Args) < 2 {
-		usage("no command specified")
-	}
-
-	cmd := strings.ToLower(os.Args[1])
-	switch cmd {
-	case "run":
-		app.Start()
-	case "install":
-		err = app.Service.InstallService("Bakery backend service")
-	case "remove":
-		err = app.Service.RemoveService()
-	case "start":
-		err = app.Service.StartService()
-		if err == nil {
-			log.Info("Service stared")
-		}
-	case "stop":
-		err = app.Service.ControlService(svc.Stop, svc.Stopped)
-		if err == nil {
-			log.Info("Service stopped")
-		}
-	case "pause":
-		err = app.Service.ControlService(svc.Pause, svc.Paused)
-		if err == nil {
-			log.Info("Service paused")
-		}
-	case "continue":
-		err = app.Service.ControlService(svc.Continue, svc.Running)
-		if err == nil {
-			log.Info("Service continued")
-		}
-	default:
-		usage(fmt.Sprintf("invalid command %s", cmd))
-	}
-	if err != nil {
-		log.Fatalf("failed to %s %s: %v", cmd, app.Service.Name, err)
-	}
-
+	app.Start()
 }
--- application/internal/app/app.go
+++ application/internal/app/app.go
@@ -6,7 +6,6 @@ import (
 	"bakery/application/internal/config"
 	"bakery/application/internal/domain/bakery"
 	"bakery/application/internal/ports"
-	"bakery/application/internal/service"
 	"context"
 	"os"
 	"os/signal"
@@ -17,16 +16,12 @@ import (
 )
 
 type App struct {
-	Service    *service.MyService
 	Httpserver *httpserver.Server
 	Storage    ports.Storage
-	InService  bool
 }
 
-func New(inService bool, log *logrus.Logger) *App {
-	app := App{
-		InService: inService,
-	}
+func New(log *logrus.Logger) *App {
+	app := App{}
 	logger := logrus.New()
 	config := config.ReadConfig()
 	storage := pgstorage.New(logger, config)
@@ -34,26 +29,19 @@ func New(inService bool, log *logrus.Logger) *App {
 	mHttpserver := httpserver.New(config, mBakery, logger)
 	app.Httpserver = mHttpserver
 	app.Storage = storage
-	if inService {
-		app.Service = service.New(mHttpserver, logger, storage, "bakery")
-	}
 	return &app
 }
 
 func (app *App) Start() {
-	if app.InService {
-		app.Service.Start(context.Background())
-	} else {
-		ctx, cancelFunc := context.WithCancel(context.Background())
-		storageStarted := make(chan int)
-		go app.Storage.Start(ctx, storageStarted)
-		<-storageStarted
-		go app.Httpserver.Start(ctx)
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-		<-c
-		cancelFunc()
-		<-time.After(10 * time.Second)
-	}
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	storageStarted := make(chan int)
+	go app.Storage.Start(ctx, storageStarted)
+	<-storageStarted
+	go app.Httpserver.Start(ctx)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-c
+	cancelFunc()
+	<-time.After(10 * time.Second)
 
 }
